299600400be3445e1f2582231e4d419e3b27d3ce
deepak22 <deepakchauhan221997@gmail.com>
2020-08-24 15:10:59 +0530

    'build'

---
+++ src/build/precache-manifest.1a08aff8e7ab051ed96c13dc46c1a052.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1eab405c38c73608a40cd4b9b8d36f",
+    "url": "/covidtrak/index.html"
+  },
+  {
+    "revision": "35250cb5a535a2a92748",
+    "url": "/covidtrak/static/css/2.40e20e49.chunk.css"
+  },
+  {
+    "revision": "1a1628db167336c4a7e8",
+    "url": "/covidtrak/static/css/main.89e3a179.chunk.css"
+  },
+  {
+    "revision": "35250cb5a535a2a92748",
+    "url": "/covidtrak/static/js/2.99a40849.chunk.js"
+  },
+  {
+    "revision": "1dc8c40d57b41eea617d6ecda1b3a19f",
+    "url": "/covidtrak/static/js/2.99a40849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1628db167336c4a7e8",
+    "url": "/covidtrak/static/js/main.d0f93b62.chunk.js"
+  },
+  {
+    "revision": "1f3583ec45dd537c8641",
+    "url": "/covidtrak/static/js/runtime-main.468482ec.js"
+  },
+  {
+    "revision": "42949f4421195377af5a4373fd2b9162",
+    "url": "/covidtrak/static/media/cough.42949f44.svg"
+  },
+  {
+    "revision": "e16a155234d70cdc6a8648c25750632a",
+    "url": "/covidtrak/static/media/covid.e16a1552.svg"
+  },
+  {
+    "revision": "96d04938c5b366419038dd128093c3c9",
+    "url": "/covidtrak/static/media/covidactive.96d04938.svg"
+  },
+  {
+    "revision": "742a04b3251a1f6627e4e17e36dc524a",
+    "url": "/covidtrak/static/media/coviddeath.742a04b3.svg"
+  },
+  {
+    "revision": "13589c7e6721d83fab615bc642fce352",
+    "url": "/covidtrak/static/media/covidrecover.13589c7e.svg"
+  },
+  {
+    "revision": "4c6822d3e554ac04cdea40319ac4583a",
+    "url": "/covidtrak/static/media/face-mask.4c6822d3.svg"
+  },
+  {
+    "revision": "ffe8659e9f90303c5636c00e23c5440d",
+    "url": "/covidtrak/static/media/fever.ffe8659e.svg"
+  },
+  {
+    "revision": "5d21c6d0401341c772fe520d25cdf944",
+    "url": "/covidtrak/static/media/fight.5d21c6d0.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covidtrak/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covidtrak/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covidtrak/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covidtrak/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covidtrak/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "13032f25b02f57a613f6d2748e28726c",
+    "url": "/covidtrak/static/media/hero.13032f25.png"
+  },
+  {
+    "revision": "d906ac213eb78b53fc711c73be8265aa",
+    "url": "/covidtrak/static/media/homestay.d906ac21.png"
+  },
+  {
+    "revision": "745c5655c7260608903308efa70186d7",
+    "url": "/covidtrak/static/media/pandemic.745c5655.png"
+  },
+  {
+    "revision": "586260e7bf5b7c2676ecba93dfd9dc85",
+    "url": "/covidtrak/static/media/protection.586260e7.png"
+  },
+  {
+    "revision": "052768334c109d8a67b59b495509219c",
+    "url": "/covidtrak/static/media/socialdist.05276833.svg"
+  },
+  {
+    "revision": "358b15f9753556213fd817942c85ef64",
+    "url": "/covidtrak/static/media/stay-safe.358b15f9.png"
+  },
+  {
+    "revision": "a319194d44d229465d1e5580d7bd168a",
+    "url": "/covidtrak/static/media/wash-hands.a319194d.svg"
+  }
+]);
